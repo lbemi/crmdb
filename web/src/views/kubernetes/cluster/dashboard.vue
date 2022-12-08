3adf197244b29399fe24357ac8af7e76b4d43a5f
lei <81576870@qq.com>
2022-12-08 21:00:43 +0800

    添加echart

--- web/src/views/kubernetes/cluster/dashboard.vue
+++ web/src/views/kubernetes/cluster/dashboard.vue
@@ -1,104 +1,105 @@
 /** * Created by lei on 2022/12/08 */
 <template>
-  <div>
-    <table class="info">
-      <tr>
-        <td colspan="3" class="title">基本信息</td>
-      </tr>
-      <tr>
-        <td>集群ID: {{ data.cluster.id }}</td>
-        <td colspan="2">
-          <div v-if="data.cluster.status == true">
-            <div
-              style="
-                display: inline-block;
-                width: 12px;
-                height: 12px;
-                background: #67c23a;
-                border-radius: 50%;
-              "
-            ></div>
-            <span style="margin-left: 5px; font-size: 12px; color: #67c23a"
-              >运行中
-            </span>
-          </div>
-          <div v-else>
-            <div
-              style="
-                display: inline-block;
-                width: 12px;
-                height: 12px;
-                background: #f56c6c;
-                border-radius: 50%;
-              "
-            ></div>
-            <span style="margin-left: 5px; font-size: 12px; color: #f56c6c"
-              >故障
-            </span>
-          </div>
-        </td>
-      </tr>
-    </table>
+  <div style="display: flex;justify-content: space-between;">
+    <el-card id="echart" style="width: 400px; height: 200px" />
+    <el-card id="echart1" style="width: 400px; height: 200px" />
+    <el-card id="echart2" style="width: 400px; height: 200px" />
   </div>
-  <div style="margin-top: 15px">
-    <table class="info">
-      <tr>
-        <td colspan="3" class="title">基本信息</td>
-      </tr>
-      <tr>
-        <td>API Server 内网连接端点</td>
-        <td colspan="2">http://{{ data.cluster.internal_ip }}</td>
-      </tr>
-      <tr>
-        <td>Pod 网络 CIDR</td>
-        <td colspan="2">{{ data.cluster.pod_cidr }}</td>
-      </tr>
-      <tr>
-        <td>Service CIDR</td>
-        <td colspan="2">10.244.0.0/24</td>
-      </tr>
-      <tr>
-        <td>运行时</td>
-        <td colspan="2">{{ data.cluster.runtime }}</td>
-      </tr>
-      <tr>
-        <td>节点 IP 数量</td>
-        <td colspan="2">{{ data.cluster.nodes }}</td>
-      </tr>
-      <tr>
-        <td>网络插件</td>
-        <td colspan="2">Calico</td>
-      </tr>
-      <tr>
-        <td>创建时间</td>
-        <td colspan="2">{{ $filters.dateFormat(data.cluster.created_at) }}</td>
-      </tr>
-    </table>
-  </div>
+    
 </template>
 
 <script setup lang="ts">
-import { clusterInfo } from '@/type/cluster'
-import { onMounted, reactive } from 'vue'
-import { clusterApi } from '@/views/container/api'
-import { kubeStore } from '@/store/kubernetes/kubernetes'
-
-const clu = kubeStore()
-const data = reactive({
-  cluster: {} as clusterInfo,
-  query: {
-    name: ''
-  }
-})
+import {onMounted} from 'vue';
+import {ECharts,EChartOption, init} from 'echarts';
 
 onMounted(() => {
-  getCluster()
-})
-const getCluster = async () => {
-  data.query.name = clu.activeCluster
-  await clusterApi.get.request(data.query).then((res) => {
-    data.cluster = res.data
-  })
+  app1()
+  app2()
+  app3()
+});
+
+const app1 = ()=>{
+  const charEle = document.getElementById('echart') as HTMLElement;
+  const charEch: ECharts = init(charEle);
+  const option: EChartOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+      }
+    ]
+  };
+  charEch.setOption(option);
+}
+const app2 = ()=>{
+  const charEle = document.getElementById('echart2') as HTMLElement;
+  const charEch: ECharts = init(charEle);
+  const option: EChartOption = {
+    tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    top: '5%',
+    left: 'center'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: ['40%', '70%'],
+      avoidLabelOverlap: false,
+      label: {
+        show: false,
+        position: 'center'
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: 40,
+          fontWeight: 'bold'
+        }
+      },
+      labelLine: {
+        show: false
+      },
+      data: [
+        { value: 1048, name: 'Search Engine' },
+        { value: 735, name: 'Direct' },
+        { value: 580, name: 'Email' },
+        { value: 484, name: 'Union Ads' },
+        { value: 300, name: 'Video Ads' }
+      ]
+    }
+  ]
+  };
+  charEch.setOption(option);
+}
+const app3 = ()=>{
+  const charEle = document.getElementById('echart1') as HTMLElement;
+  const charEch: ECharts = init(charEle);
+  const option: EChartOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+      }
+    ]
+  };
+  charEch.setOption(option);
 }
 </script>
 
