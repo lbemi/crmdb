f237553a618eafe9b694348771f013b11374d781
lei <934366858@qq.com>
2023-03-21 18:03:28 +0800

    添加pod页面

--- web/src/views/kubernetes/api.ts
+++ web/src/views/kubernetes/api.ts
@@ -56,3 +56,21 @@ export const eventApi = {
   list: Api.create('/event/{namespace}', 'get'),
   get: Api.create('/event/{namespace}/{name}', 'get')
 }
+
+/*
+	//pod 资源路由
+	pod := group.Group("/pod")
+	{
+		pod.GET("/:namespace", cloud.ListPods)
+		pod.GET("/:namespace/:podName", cloud.GetPod)
+		pod.POST("", cloud.CreatePod)
+		pod.PUT("", cloud.UpdatePod)
+		pod.DELETE("/:namespace/:podName", cloud.DeletePod)
+	}
+*/
+export const podApi = {
+  list: Api.create('/pod/{namespace}', 'get'),
+  get: Api.create('/pod/{namespace}/{podName}', 'get'),
+  delete: Api.create('/pod/{namespace}/{podName}', 'delete'),
+  create: Api.create('/pod', 'post')
+}
