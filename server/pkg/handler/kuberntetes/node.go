cdfb623b3edd969be3afaac811dfa24e5c3f5b9e
lei <934366858@qq.com>
2023-03-23 18:08:00 +0800

    完善

--- server/pkg/handler/kuberntetes/node.go
+++ server/pkg/handler/kuberntetes/node.go
@@ -5,10 +5,11 @@ import (
 	"encoding/json"
 	"github.com/lbemi/lbemi/pkg/bootstrap/log"
 	"github.com/lbemi/lbemi/pkg/common/store"
+	"github.com/lbemi/lbemi/pkg/handler/types"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/types"
+	apitype "k8s.io/apimachinery/pkg/types"
 	"sort"
 )
 
@@ -17,7 +18,7 @@ type NodeGetter interface {
 }
 
 type INode interface {
-	List(ctx context.Context) ([]*v1.Node, error)
+	List(ctx context.Context) ([]*types.Node, error)
 	Get(ctx context.Context, name string) (*v1.Node, error)
 	Delete(ctx context.Context, name string) error
 	Create(ctx context.Context, node *v1.Node) (*v1.Node, error)
@@ -31,16 +32,37 @@ type node struct {
 	cli *store.Clients
 }
 
-func (n *node) List(ctx context.Context) ([]*v1.Node, error) {
+func (n *node) List(ctx context.Context) ([]*types.Node, error) {
+	var nodes = make([]*types.Node, 0)
 	nodeList, err := n.cli.SharedInformerFactory.Core().V1().Nodes().Lister().List(labels.Everything())
+	for _, node := range nodeList {
+		cpuUsage, memoryUsage, err := n.GetNodeUsage(ctx, node)
+		if err != nil {
+			log.Logger.Error("获取节点资源使用情况失败", err)
+		}
+		podNum := n.getPodNumByNode(ctx, node.Name)
+		item := &types.Node{
+			TypeMeta:   node.TypeMeta,
+			ObjectMeta: node.ObjectMeta,
+			Spec:       node.Spec,
+			Status:     node.Status,
+			Usage: types.Usage{
+				Cpu:    cpuUsage,
+				Memory: memoryUsage,
+				Pod:    podNum,
+			},
+		}
+		nodes = append(nodes, item)
+	}
 	// 按创建时间排序排序
-	sort.Slice(nodeList, func(i, j int) bool {
-		return nodeList[j].ObjectMeta.GetCreationTimestamp().Time.Before(nodeList[i].ObjectMeta.GetCreationTimestamp().Time)
+	sort.Slice(nodes, func(i, j int) bool {
+		return nodes[j].ObjectMeta.GetCreationTimestamp().Time.After(nodes[i].ObjectMeta.GetCreationTimestamp().Time)
 	})
+
 	if err != nil {
 		log.Logger.Error(err)
 	}
-	return nodeList, err
+	return nodes, err
 }
 
 func (n *node) Get(ctx context.Context, name string) (*v1.Node, error) {
@@ -84,7 +106,7 @@ func (n *node) Patch(ctx context.Context, name string, labels map[string]string)
 		return nil, err
 	}
 
-	res, err := n.cli.ClientSet.CoreV1().Nodes().Patch(ctx, name, types.StrategicMergePatchType, playLoadBytes, metav1.PatchOptions{})
+	res, err := n.cli.ClientSet.CoreV1().Nodes().Patch(ctx, name, apitype.StrategicMergePatchType, playLoadBytes, metav1.PatchOptions{})
 	if err != nil {
 		log.Logger.Error(err)
 	}
@@ -92,13 +114,32 @@ func (n *node) Patch(ctx context.Context, name string, labels map[string]string)
 }
 
 func (n *node) GetNodeUsage(ctx context.Context, node *v1.Node) (cpuUsage, memoryUsage float64, err error) {
+
 	nodeMetric, err := n.cli.MetricSet.MetricsV1beta1().NodeMetricses().Get(ctx, node.Name, metav1.GetOptions{})
 	if err != nil {
 		return
 	}
+
 	cpuUsage = float64(nodeMetric.Usage.Cpu().MilliValue()) / float64(node.Status.Capacity.Cpu().MilliValue())
 	memoryUsage = float64(nodeMetric.Usage.Memory().MilliValue()) / float64(node.Status.Capacity.Memory().MilliValue())
 	return
+}
+
+func (n *node) getPodNumByNode(ctx context.Context, nodeName string) int {
+	list, err := n.cli.SharedInformerFactory.Core().V1().Pods().Lister().Pods("").List(labels.Everything())
+	if err != nil {
+		log.Logger.Error(err)
+		return 0
+	}
+
+	var count = 0
+	for _, item := range list {
+		if item.Spec.NodeName == nodeName {
+			count++
+		}
+	}
+
+	return count
 }
 
 func NewNode(cli *store.Clients) *node {
