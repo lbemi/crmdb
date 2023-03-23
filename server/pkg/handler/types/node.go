dfccd03fbac535b90e791f8bb070d5a673ff3b4a
lei <934366858@qq.com>
2023-03-23 18:08:00 +0800

    完善

---
+++ server/pkg/handler/types/node.go
@@ -0,0 +1,20 @@
+package types
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type Node struct {
+	v1.TypeMeta   `json:",inline"`
+	v1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	Spec          corev1.NodeSpec   `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
+	Status        corev1.NodeStatus `json:"status,omitempty" protobuf:"bytes,3,opt,name=status"`
+	Usage         `json:"usage"`
+}
+
+type Usage struct {
+	Pod    int     `json:"pod"`
+	Cpu    float64 `json:"cpu"`
+	Memory float64 `json:"memory"`
+}
---
+++ server/pkg/handler/types/pod.go
@@ -0,0 +1,15 @@
+package types
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type Pod struct {
+	v1.TypeMeta   `json:",inline"`
+	v1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	Spec          corev1.PodSpec   `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
+	Status        corev1.PodStatus `json:"status,omitempty" protobuf:"bytes,3,opt,name=status"`
+	MemoryUsage   float64          `json:"memory_usage"`
+	CpuUsage      float64          `json:"cpu_usage"`
+}
