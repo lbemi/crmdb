036d758d86e8bde01a9c046089e87c9fcedbc1b9
lbemi <lbemi@lbemi.com>
2023-03-27 18:39:13 +0800

    new ui

--- web/src/request/enums.ts
+++
@@ -1,9 +0,0 @@
-export enum ResultEnum {
-  SUCCESS = 200,
-  ERROR = 400,
-  PARAM_ERROR = 405,
-  SERVER_ERROR = 500,
-  NO_PERMISSION = 5000,
-
-  USER_NOTEXIST = 1004
-}
--- web/src/views/layout/main/index.vue
+++
@@ -1,8 +0,0 @@
-/** * Created by lei on 2022/11/01 */
-<template>
-  <el-card class="box-card">Main</el-card>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped lang="less"></style>
