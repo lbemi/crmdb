4086af7b1b0d29546a164a9ad1f915f93c5f97c6
lei <934366858@qq.com>
2023-04-08 22:25:28 +0800

    jsformat

---
+++ server/jsFormat/model/model.go
@@ -0,0 +1,21 @@
+package model
+
+type OldJs struct {
+	Title  string `json:"title" yaml:"title"`
+	Field  string `json:"field" yaml:"field"`
+	Crspan string `json:"crspan" yaml:"crspan"`
+	Halign string `json:"halign" yaml:"halign"`
+	Align  string `json:"align" yaml:"align"`
+	//Visible   bool   `yaml:"visible" json:"visible"`
+	Formatter string `json:"formatter" yaml:"formatter"`
+}
+
+type NewJS struct {
+	Id     string   `json:"id"`
+	Name   string   `json:"name"`
+	Type   string   `json:"type"`
+	Custom []string `json:"custom"`
+	Width  string   `json:"width"`
+	Align  string   `json:"align"`
+	Order  string   `json:"order"`
+}
